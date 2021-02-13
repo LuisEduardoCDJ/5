a8e40830303109070c6ddd407b0a5d574318e9ac
Eduardo Castillo <thenoobprooyt12@gmail.com>
2021-02-13 00:18:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+num1 = int(input('Ingrese un numero'))
+num2 = int(input('Ingrese un numero'))
+num3 = int(input('Ingrese un numero'))
+num4 = int(input('Ingrese un numero'))
+num5 = int(input('Ingrese un numero'))
+print(num1,num2,num3,num4,num5)
